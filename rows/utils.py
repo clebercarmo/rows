9aa290e69df788cc18229533bc7bbfe20c2c5002
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2017-12-06 03:48:37 -0200

    Make the PDF plugin detectable

--- rows/utils.py
+++ rows/utils.py
@@ -94,6 +94,7 @@ FILE_EXTENSIONS = {
         'txt': 'text/txt',
         'xls': 'application/vnd.ms-excel',
         'xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'pdf': 'application/pdf',
 }
 MIME_TYPE_TO_PLUGIN_NAME = {
         'application/json': 'json',
@@ -105,6 +106,7 @@ MIME_TYPE_TO_PLUGIN_NAME = {
         'text/csv': 'csv',
         'text/html': 'html',
         'text/txt': 'txt',
+        'application/pdf': 'pdf',
 }
 regexp_sizes = re.compile('([0-9,.]+ [a-zA-Z]+B)')
 MULTIPLIERS = {'B': 1, 'KiB': 1024, 'MiB': 1024 ** 2, 'GiB': 1024 ** 3}
